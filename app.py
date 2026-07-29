ebcaa0868801f983a53053f4ee361abd9e6d109a
FreddieSawiras <freddiesawiras2@gmail.com>
2026-07-29 13:09:31 -0400

    Update app

--- app.py
+++ app.py
@@ -1718,10 +1718,4 @@ st.markdown(
 )
 
 if st.session_state.autoplay:
-    time.sleep(3)
-    idx = st.session_state.scenario_idx % len(SCENARIOS)
-    scenario = SCENARIOS[idx]
-    for _k, _v in scenario.items():
-        st.session_state[_k] = _v
-    st.session_state.scenario_idx = idx + 1
-    st.rerun()
+    time.sleep
